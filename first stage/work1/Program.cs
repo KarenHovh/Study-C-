c3145eb98c35759863f049ce3aa2a28e2fd44024
ISTC CREATE LAB PC <zorayrbaghdasaryan@gmail.com>
2018-09-24 20:58:24 +0400

    add first app

---
+++ first stage/work1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 15;
+            char sim = args;
+            string s = "name";
+            float k = 15.12f;
+            double d = 123;
+        }
+    }
+}
