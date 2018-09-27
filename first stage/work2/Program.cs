9c88d5a6374daf93213c9d5d947026f31ff07d41
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-09-27 11:46:05 +0400

    arithmetic and geometric means

--- first stage/work2/Program.cs
+++ first stage/work2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// This program calculates the arithmetic and geometric means of two numbers
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace work2
     {
         static void Main(string[] args)
         {
+            Console.Write("enter value of x = "); 
+            string t=Console.ReadLine();
+            double x = Convert.ToDouble(t);
+            Console.Write("enter value of y = ");
+            t = Console.ReadLine();
+            double y = Convert.ToDouble(t);
+            double am = (x + y) / 2;
+            Console.WriteLine("arithmetic mean is equal " + am);
+            double gm = Math.Sqrt(x * y);
+            Console.WriteLine("geometric mean is equal " + gm);
+            Console.ReadKey();
+                                                
         }
     }
 }
