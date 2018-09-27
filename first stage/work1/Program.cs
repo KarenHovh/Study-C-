c3c6bf83c8f614f7385793ab94ca67c66bac8aea
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-09-27 12:47:47 +0400

    arithmetic and geometric means

--- first stage/work1/Program.cs
+++ first stage/work1/Program.cs
@@ -10,11 +10,7 @@ namespace work1
     {
         static void Main(string[] args)
         {
-            int number = 15;
-            char sim = args;
-            string s = "name";
-            float k = 15.12f;
-            double d = 123;
+       
         }
     }
 }
